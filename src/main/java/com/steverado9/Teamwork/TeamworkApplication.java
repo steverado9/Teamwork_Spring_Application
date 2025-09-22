207ad08906a3ba77916869b91c491bce9db587d8
stephen <isaacsteverado9@gmail.com>
2025-09-22 02:31:37 +0100

    new commit

---
+++ src/main/java/com/steverado9/Teamwork/TeamworkApplication.java
@@ -0,0 +1,13 @@
+package com.steverado9.Teamwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamworkApplication.class, args);
+	}
+
+}
